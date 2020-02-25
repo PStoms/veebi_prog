bdc01fdd9f558bcedbd7d2a5c674048b01b65ea2
Patrick <p.toms@khk.ee>
2020-02-25 09:23:19 +0200

    bootstrap loodud

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -5,8 +5,8 @@ class Controller
 {
 //load view
     public function view($viewFile, $data = array()){
-        if(file_exists('../app/views'.$viewFile.'.php')){
-        require_once '../app/views'.$viewFile.'.php';
+        if(file_exists('../app/views/'.$viewFile.'.php')){
+        require_once '../app/views/'.$viewFile.'.php';
         }else{
             die($viewFile.'.php does not exist');
         }
