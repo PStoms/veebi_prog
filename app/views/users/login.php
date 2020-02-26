9a9ca8f14d886edb7b8d0546d3d4f2578e585729
Patrick <p.toms@khk.ee>
2020-02-26 10:21:54 +0200

    vormi loomie valmis koos veateadetega

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,17 +1,17 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-<!--login vorm -->
 <div class="row">
     <div class="col-md-6 mx-auto">
-        <div class="card card-body bg-light mt-5">
-            <h2>Log in</h2>
+        <div class="card card/body bg-light mt-5">
+            <h2>Log-In</h2>
             <form action="<?php echo URLROOT;?>/users/login" method="post">
+
                 <div class="form-group">
-                    <label for="meil">Email: <sup>*</sup></label>
-                    <input type="email" id="meil" class="form-control">
+                    <label for="name">Email: <sup>*</sup></label>
+                    <input type="email" id="email" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label for="pass">Password: <sup>*</sup></label>
+                    <label for="name">Password: <sup>*</sup></label>
                     <input type="password" id="pass" class="form-control">
                 </div>
 
@@ -20,7 +20,9 @@
                         <input type="submit" value="Login" class="btn btn-block bg-info">
                     </div>
                 </div>
+            </form>
         </div>
     </div>
 </div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
+
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,34 +1,39 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-<!--register vorm -->
 <div class="row">
     <div class="col-md-6 mx-auto">
-        <div class="card card-body bg-light mt-5">
-        <h2>Create an Account</h2>
+        <div class="card card/body bg-light mt-5">
+            <h2>Register</h2>
             <form action="<?php echo URLROOT;?>/users/register" method="post">
                 <div class="form-group">
                     <label for="name">Name: <sup>*</sup></label>
-                    <input type="text" id="name" class="form-control <?php echo (!empty($data['name_err'])) ?  'is-invalid' : '';?>" name="name">
+                        <input type="text" id="name" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : '';?>" name="name">
                     <span class="invalid-feedback"><?php echo $data['name_err'];?></span>
                 </div>
 
+                <div class="form-group">
+                    <label for="name">Email: <sup>*</sup></label>
+                    <input type="email" id="email" class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : '';?>" name="email">
+                    <span class="invalid-feedback"><?php echo $data['email_err'];?></span>
+                </div>
 
                 <div class="form-group">
-                    <label for="pass">Password: <sup>*</sup></label>
-                    <input type="password" id="pass" class="form-control <?php echo (!empty($data['pass_err'])) ?  'is-invalid' : '';?>" name="pass">
+                    <label for="name">Password: <sup>*</sup></label>
+                    <input type="password" id="pass" class="form-control <?php echo (!empty($data['pass_err'])) ? 'is-invalid' : '';?>" name="pass">
                     <span class="invalid-feedback"><?php echo $data['pass_err'];?></span>
                 </div>
 
-
                 <div class="form-group">
-                    <label for="meil">Email: <sup>*</sup></label>
-                    <input type="email" id="meil" class="form-control <?php echo (!empty($data['meil_err'])) ?  'is-invalid' : '';?>" name="meil">
-                    <span class="invalid-feedback"><?php echo $data['meil_err'];?></span>
+                    <label for="name">Confirm password: <sup>*</sup></label>
+                    <input type="password" id="pass2" class="form-control <?php echo (!empty($data['pass2_err'])) ? 'is-invalid' : '';?>" name="pass2">
+                    <span class="invalid-feedback"><?php echo $data['pass2_err'];?></span>
                 </div>
-            <div class="row">
-                <div class="col">
-                    <input type="submit" value="Register" class="btn btn-block bg-info">
+
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Register" class="btn btn-block bg-info">
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
