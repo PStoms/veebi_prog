8b2ad27627eff89b2d55b9c53a369e69c4f60260
Patrick <p.toms@khk.ee>
2020-02-26 09:45:27 +0200

    vormi loomine ja mõllamine

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,4 +1,26 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-login
-<?php require_once APPROOT.'/views/inc/footer.php';?>
+<!--login vorm -->
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+            <h2>Log in</h2>
+            <form action="<?php echo URLROOT;?>/users/login" method="post">
+                <div class="form-group">
+                    <label for="meil">Email: <sup>*</sup></label>
+                    <input type="email" id="meil" class="form-control">
+                </div>
 
+                <div class="form-group">
+                    <label for="pass">Password: <sup>*</sup></label>
+                    <input type="password" id="pass" class="form-control">
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Login" class="btn btn-block bg-info">
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<?php require_once APPROOT.'/views/inc/footer.php';?>
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,4 +1,36 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-register
+<!--register vorm -->
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+        <h2>Create an Account</h2>
+            <form action="<?php echo URLROOT;?>/users/register" method="post">
+                <div class="form-group">
+                    <label for="name">Name: <sup>*</sup></label>
+                    <input type="text" id="name" class="form-control <?php echo (!empty($data['name_err'])) ?  'is-invalid' : '';?>" name="name">
+                    <span class="invalid-feedback"><?php echo $data['name_err'];?></span>
+                </div>
+
+
+                <div class="form-group">
+                    <label for="pass">Password: <sup>*</sup></label>
+                    <input type="password" id="pass" class="form-control <?php echo (!empty($data['pass_err'])) ?  'is-invalid' : '';?>" name="pass">
+                    <span class="invalid-feedback"><?php echo $data['pass_err'];?></span>
+                </div>
+
+
+                <div class="form-group">
+                    <label for="meil">Email: <sup>*</sup></label>
+                    <input type="email" id="meil" class="form-control <?php echo (!empty($data['meil_err'])) ?  'is-invalid' : '';?>" name="meil">
+                    <span class="invalid-feedback"><?php echo $data['meil_err'];?></span>
+                </div>
+            <div class="row">
+                <div class="col">
+                    <input type="submit" value="Register" class="btn btn-block bg-info">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
 
