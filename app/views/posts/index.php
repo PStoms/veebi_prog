d37bf1b28c3014ca27c42d22bf4a8dc946904c90
Patrick <p.toms@khk.ee>
2020-03-09 12:04:27 +0200

    testimised

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,4 +1,34 @@
-<?php require_once APPROOT.'/views/inc/header.php';?>
-all posts of my blog
+<?php require_once APPROOT.'/views/inc/header.php';
+session_start();
+include_once("db.php");
+?>
+<body>
+<h3>all posts of my blo</h3>
+<?php
+    $sql = "SELECT * FROM posts ORDER BY id DESC";
+
+    $res = mysqli_query($db, $sql) or die(mysqli_error());
+
+    $posts ='';
+
+    if(mysqli_num_rows($res) > 0) {
+        while($row = mysqli_fetch_assoc($res)) {
+            $id = $row['id'];
+            $title = $row['title'];
+            $content = $row['content'];
+            $created_at = $row['created_at'];
+
+            $output = $content;
+
+            $posts .= "<div><h2><a href='view_post.php?pid=$id' </h2><h3>$created_at</h3><p>$output</p></div> ";
+        }
+        echo $posts;
+    } else {
+            echo "There are no posts";
+    }
+?>
+</body>
+
+
 <?php require_once APPROOT.'/views/inc/footer.php';?>
 
